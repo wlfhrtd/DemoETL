ac64d2db09be0fd3400d9316c1b426d5c5cfb1aa
wlfhrtd <wlfhrtd@gmail.com>
2026-06-02 20:42:21 +0300

    Restore missing FnsFileNameGenerator

---
+++ src/DemoETL/Application/Output/FnsFileNameGenerator.cs
@@ -0,0 +1,15 @@
+﻿using DemoETL.Domain.Models;
+
+
+namespace DemoETL.Application.Output
+{
+    public static class FnsFileNameGenerator
+    {
+        public static string GenerateCanonicalName(string documentType, ImportDeclaration model)
+        {
+            var date = DateTime.Now.ToString("yyyyMMdd");
+
+            return $"ON_{documentType}_{model.TaxAuthority}_{model.TaxAuthority}_{model.Inn}{model.Kpp}_{date}_{model.IdFileSuffix}.xml";
+        }
+    }
+}
